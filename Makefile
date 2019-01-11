b2b1c02f189ceaefac0176591e9515c6ed3a1e71
rexim <reximkut@gmail.com>
2019-01-12 00:30:47 +0700

    Add console C unit

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-multik.opt: src/multik.cmx src/anime.cmx
-	ocamlopt -I ./src/ -o multik.opt src/multik.cmx src/anime.cmx
+multik.opt: src/multik.cmx src/anime.cmx src/console.o
+	ocamlopt -I ./src/ -o multik.opt src/console.o src/multik.cmx src/anime.cmx -ccopt "$(shell pkg-config --cflags --libs sdl2)"
 
 src/multik.cmx: src/multik.ml
 	ocamlopt -c -I ./src/ src/multik.ml
 
 src/anime.cmx: src/anime.ml 
 	ocamlopt -c -I ./src/ src/anime.ml 
+
+src/console.o: src/console.c
+	ocamlopt -c src/console.c -ccopt "$(shell pkg-config --cflags --libs sdl2)"
+	mv console.o src/
---
+++ src/console.c
@@ -0,0 +1,50 @@
+#include <SDL2/SDL.h>
+#include <caml/mlvalues.h>
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+CAMLprim value
+console_init(value width, value height)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    if (window == NULL) {
+        window = SDL_CreateWindow(
+            "Multik",
+            100, 100,
+            Int_val(width), Int_val(height),
+            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+        if (window == NULL) {
+            /* TODO: multik.c does not raise an error when failed to create an SDL window */
+            return Val_unit;
+        }
+    }
+
+    if (renderer == NULL) {
+        renderer = SDL_CreateRenderer(
+            window, -1,
+            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        if (renderer == NULL) {
+            /* TODO: multik.c does not raise an error when failed to create an SDL renderer */
+            return Val_unit;
+        }
+    }
+}
+
+CAMLprim value
+console_free(value unit)
+{
+    if (renderer != NULL) {
+        SDL_DestroyRenderer(renderer);
+        renderer = NULL;
+    }
+
+    if (window != NULL) {
+        SDL_DestroyWindow(window);
+        window = NULL;
+    }
+
+    SDL_Quit();
+
+    return Val_unit;
+}
