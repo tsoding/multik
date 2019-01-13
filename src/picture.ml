815e7a7c7029a37e33d0dd75ef4bc8d3c3925d03
rexim <reximkut@gmail.com>
2019-01-13 09:56:42 +0700

    (#4) typo

--- src/picture.ml
+++ src/picture.ml
@@ -7,10 +7,10 @@ let render (p: picture): unit =
   let rec render_with_context (c: Color.t) (p: picture): unit =
     match p with
     | Nothing -> ()
-    | Rect (x, y, h, w) ->
+    | Rect (x, y, w, h) ->
        let (r, g, b) = c
        in Console.set_fill_color r g b;
-          Console.fill_rect x y h w
+          Console.fill_rect x y w h
     | Compose ps ->
        List.iter (render_with_context c) ps
     | Color (c1, p1) ->
