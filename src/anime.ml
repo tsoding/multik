4834f909cfa8cd7f595fbcdb101aaee493cb954a
rexim <reximkut@gmail.com>
2019-01-13 09:05:31 +0700

    (#4) Picture prefix

--- src/anime.ml
+++ src/anime.ml
@@ -1,11 +1,9 @@
-open Picture
-
 module T = Multik.MakeMultik(
                struct
                  (* TODO(#3): Sample animation doesn't render anything substantial *)
                  type t = unit
                  let init = ()
-                 let render _ = Nothing
+                 let render _ = Picture.Nothing
                  let update delta_time s =
                    delta_time |> string_of_float |> print_endline;
                    s
