51f7bcf5ea8a3d04c19b2dc72cb086d8b1df3961
rexim <reximkut@gmail.com>
2019-01-24 17:24:01 +0700

    (#21) factor out context color

--- src/console.ml
+++ src/console.ml
@@ -11,25 +11,21 @@ external stop_cairo_render: unit -> unit = "stop_cairo_render"
 
 let renderPicture (p: Picture.t): unit =
   let rec render_with_context (c: Color.t) (p: Picture.t): unit =
+    let (r, g, b) = c in
+    set_fill_color r g b;
     match p with
     | Nothing -> ()
     | Rect (x, y, w, h) ->
-       let (r, g, b) = c
-       in set_fill_color r g b;
-          fill_rect x y w h
+       fill_rect x y w h
     | Compose ps ->
        List.iter (render_with_context c) ps
     | Color (c1, p1) ->
        render_with_context c1 p1
     | Circle ((x, y), radius) ->
-       let (r, g, b) = c
-       in set_fill_color r g b;
-          fill_circle x y radius
+       fill_circle x y radius
     (* TODO(#21): Picture.Text is not interpreted in Console.renderPicture *)
     | Text ((x, y), _) ->
-       let (r, g, b) = c
-       in set_fill_color r g b;
-          fill_rect x y 200.0 50.0
+       fill_rect x y 200.0 50.0
   in start_cairo_render ();
      clear 0.0 0.0 0.0;
      render_with_context Color.black p;
