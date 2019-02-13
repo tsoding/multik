491351c1203c883083bb382f5f8cda6fdf840c07
rexim <reximkut@gmail.com>
2019-02-14 00:29:06 +0700

    Add TODO(#45)

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,5 @@
 let () =
-  (* TODO: animation file path is hardcoded *)
+  (* TODO(#45): animation file path is hardcoded *)
   Dynlink.loadfile("./src/sample.cmo");
   Multik.run ();
   print_endline "done"
