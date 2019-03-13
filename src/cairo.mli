adb09a30af1a1ec8e7910323099550b80b927aff
rexim <reximkut@gmail.com>
2019-03-14 06:11:49 +0700

    Make Cairo.boundary private
    It's not used outside of the module anyway

--- src/cairo.mli
+++ src/cairo.mli
@@ -5,4 +5,3 @@ val with_texture : SdlTexture.t -> (t -> 'a) -> 'a
 
 external save_to_png : t -> string -> unit = "multik_cairo_save_to_png"
 val render : t -> Picture.t -> unit
-val boundary : t -> Picture.t -> Rect.t
