7b9a742b26da559fbd70827258051a24b4d6631b
rexim <reximkut@gmail.com>
2019-03-21 04:43:32 +0700

    Remove TODO(#57)

--- src/console.ml
+++ src/console.ml
@@ -1,7 +1,6 @@
 open Picture
 
 (* TODO(#35): The project mixes up camel case and snake case for function names *)
-(* TODO(#57): chess pattern is broken *)
 
 external init: int -> int -> unit = "console_init"
 external free: unit -> unit = "console_free"
