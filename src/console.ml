a1ad8128be9ea97ebd612ee6b0f319e32b2a83b8
rexim <reximkut@gmail.com>
2019-03-21 14:04:05 +0700

    (#35) renderPicture -> render_picture

--- src/console.ml
+++ src/console.ml
@@ -1,14 +1,12 @@
 open Picture
 
-(* TODO(#35): The project mixes up camel case and snake case for function names *)
-
 external init: int -> int -> unit = "console_init"
 external free: unit -> unit = "console_free"
 external should_quit: unit -> bool = "console_should_quit"
 external present: unit -> unit = "console_present"
 external texture: unit -> SdlTexture.t = "console_texture"
 
-let renderPicture (p: Picture.t): unit =
+let render_picture (p: Picture.t): unit =
   Cairo.with_texture (texture ())
     (fun c ->
       Cairo.fill_chess_pattern c;
